ca12ef17d6983c38db3d4ad44d410df4e1b4b91a
codeEmpress1 <halof.halima@gmail.com>
2026-08-13 06:11:40 +0300

    create calculator and add dependencies

---
+++ lesson_2/calculator.js
@@ -0,0 +1,56 @@
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt("Welcome to Calculator!");
+// Ask the user for the first number.
+prompt("What's the first number?");
+let firstNumber = readline.question("First number: ");
+// validate that the input is a number
+while (invalidNumber(firstNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  firstNumber = readline.question("First number: ");
+}
+// Ask the user for the second number.
+prompt("What's the second number?");
+let secondNumber = readline.question("Second number: ");
+// validate that the input is a number
+while (invalidNumber(secondNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  secondNumber = readline.question("Second number: ");
+}
+// Ask the user for an operation to perform.
+prompt("What operation would you like to perform?");
+let operation = readline.question("Operation (+, -, *, /): ");
+// validate that the input is a valid operation
+while (!['+', '-', '*', '/'].includes(operation)) {
+  prompt("Must choose one of the following operations: +, -, *, / ");
+  operation = readline.question("Operation (+, -, *, /): ");
+}
+// Perform the operation on the two numbers.
+let result;
+switch (operation) {
+  case "+":
+    result = Number(firstNumber) + Number(secondNumber);
+    break;
+  case "-":
+    result = Number(firstNumber) - Number(secondNumber);
+    break;
+  case "*":
+    result = Number(firstNumber) * Number(secondNumber);
+    break;
+  case "/":
+    result = Number(firstNumber) / Number(secondNumber);
+    break;
+  default:
+    console.log("Invalid operation.");
+    return;
+}
+// Print the result to the terminal.
+console.log(`The result is: ${result}`);
