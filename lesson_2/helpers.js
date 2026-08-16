d72cda46915697803fd6f1984611ac03f2f0ecc2
codeEmpress1 <halof.halima@gmail.com>
2026-08-16 14:15:52 +0300

    extract helpers functions

---
+++ lesson_2/helpers.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+function getNumberInput(promptMessage, numberType) {
+  prompt(promptMessage);
+  let number = readline.question(numberType);
+  while (invalidNumber(number)) {
+    prompt(MESSAGES.en.invalid_input);
+    number = readline.question(numberType);
+  }
+  return Number(number);
+}
+
+module.exports = {
+  prompt,
+  invalidNumber,
+  getNumberInput
+};
